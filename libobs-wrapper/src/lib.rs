86fd2d811436d624c24f17ad2605fbea6f5237e9
Hendrik Lind <hendrik@sshcrack.me>
2026-01-06 13:21:24 +0100

    docs: add lib crate documentation

--- libobs-wrapper/src/lib.rs
+++ libobs-wrapper/src/lib.rs
@@ -1,3 +1,12 @@
+//! This crate contains a high level API wrapper around the OBs C API.
+//! To get started, have a look at the [examples](https://github.com/libobs-rs/libobs-rs/tree/main/examples) folder.
+//!
+//! For documentation, have a look at the [docs](https://libobs-rs.github.io/libobs_wrapper/).
+//!
+//! Also have a look at the [libobs-simple](https://crates.io/crates/libobs-simple) crate, which has a lot of
+//! source builders for easier source creation.
+//! You can also create outputs easily with it.
+
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
 pub mod context;
